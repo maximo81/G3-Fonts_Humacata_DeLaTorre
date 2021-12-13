e3543ecdaa484b83ed0c03be3c0e03d9a04df9f6
Ignacio Fonts <fontsignacio@gmail.com>
2021-12-13 18:36:06 -0300

    subo primeras dos funciones

--- Modulo_de_recepcion.cpp
+++ Modulo_de_recepcion.cpp
@@ -90,6 +90,138 @@ int MenuDeOpciones()
 	return(opc);
 }
 
+void Iniciosesion(int &bandera)
+{
+	system("cls");
+	 //recuadro principal 
+	for (int x=1; x<=100; x++)
+	{  
+		gotoxy(x,0); printf("%c",177);
+		gotoxy(x,24); printf("%c",177);
+		
+		if(x<=24)
+		{
+			gotoxy(1,x); printf("%c",177); 
+			gotoxy(100,x); printf("%c",177); 
+		}
+	}
+	
+	char nameuser[10], passuser[32]; 
+	FILE *arch2;int clavecorrecta, usuariocorrecto; char usuario[32];
+	CuentasDeUsuarios usu;
+	
+	arch2=fopen("Recepcionistas.dat","r+b");
+	
+	if(arch2==NULL)
+	{
+		gotoxy(2,2); printf("Error en el programa");
+		arch2=fopen("Recepcionistas.dat","a+b");
+	}
+	else
+	{
+		gotoxy(2,4); printf("Ingrese nombre de usuario: ");
+		_flushall();
+		gets(nameuser);
+		gotoxy(2,5); printf("Clave de usuario: ");
+		_flushall();
+		gets(passuser);
+		
+		bandera=0;
+		
+		fread(&usu,sizeof(CuentasDeUsuarios),1,arch2);
+		while(!feof(arch2))
+		{
+			usuariocorrecto=strcmp(nameuser, usu.user);
+			clavecorrecta=strcmp(passuser, usu.password);
+			if(clavecorrecta==0&&usuariocorrecto==0)
+			{
+				bandera=1;
+				strcpy(usuario,usu.ApellidoNombre);
+			}
+			
+		fread(&usu,sizeof(CuentasDeUsuarios),1,arch2);
+		}
+		if(bandera==1)
+		{
+			gotoxy(2,4);printf("                                                    ");
+			gotoxy(2,5); printf("                                                    ");
+			gotoxy(2,4); printf(" Bienvenido: ");
+			gotoxy(16,4);puts(usuario);
+			gotoxy(2,5); system("pause");
+			gotoxy(2,4);printf("                                                               ");
+			gotoxy(2,5); printf("                                                              ");
+		}
+		if(bandera==0)
+		{
+			
+			gotoxy(2,4);printf("                                                    ");
+			gotoxy(2,5); printf("                                                    ");
+			gotoxy(2,4); printf("Clave o usuario incorrecto");
+			gotoxy(2,5); system("pause");
+			gotoxy(2,4);printf("                                                    ");
+			gotoxy(2,5); printf("                                                    ");
+		}
+	}
+	fclose(arch2);
+}
+
+void RegistrarCliente(int bandera)
+{
+	system("cls");
+	 //recuadro principal 
+	for (int x=1; x<=100; x++)
+	{  
+		gotoxy(x,0); printf("%c",177);
+		gotoxy(x,24); printf("%c",177);
+		
+		if(x<=24)
+		{
+			gotoxy(1,x); printf("%c",177); 
+			gotoxy(100,x); printf("%c",177); 
+		}
+	}
+	/////********************Declaracion de variables**********************////
+	FILE *arch4; Cliente reg;
+	/////********************Apertura de archivos**********************////
+	arch4=fopen("Clientes.dat","a+b");
+	/////********************Verificacion de archivos**********************////
+	if(arch4==NULL)
+	{
+		gotoxy(2,2);printf("*Ha habido errores en el sistema*"); 
+		gotoxy(2,3); exit(1);
+	}
+	/////********************Verificacion de inicio de sesion**********************////
+	if(bandera==0)
+	{
+		gotoxy(2,4); printf("No ha iniciado sesion");
+		fclose(arch4);
+		gotoxy(2,5); system("pause");
+		gotoxy(2,4);printf("                                                                      ");
+		gotoxy(2,5);printf("                                                                             ");
+		
+	}
+	else
+	{ /////********************Registro de Clientes**********************////
+		
+		gotoxy(2,4);printf("Ingrese nombre de la cliente: ");_flushall(); 
+		gotoxy(2,5);gets(reg.NombreCliente);
+		gotoxy(2,6);printf("Ingrese DNI del Cliente: "); scanf("%d",&reg.DNI_Cliente);
+		gotoxy(2,7);printf("Domicilio: "); _flushall();
+		gotoxy(2,8);gets(reg.Domicilio);
+		gotoxy(2,9);printf("Localidad: "); _flushall();
+		gotoxy(2,10);gets(reg.localidad);
+		gotoxy(2,11);printf("Telefono: "); scanf("%d",&reg.Telefono);
+		gotoxy(2,12);printf("Peso en Kg: "); scanf("%f",&reg.Peso);
+		gotoxy(2,13);printf("Fecha de nacimiento: ");
+		gotoxy(2,14);printf("Dia: "); scanf("%d",&reg.fechanacimiento.dd);
+		gotoxy(2,15);printf("Mes: "); scanf("%d",&reg.fechanacimiento.mm);
+		gotoxy(2,16);printf("Anio: "); scanf("%d",&reg.fechanacimiento.aaaa);
+		fwrite(&reg,sizeof(Cliente),1,arch4);
+		gotoxy(2,18); printf("Se ha registrado la Cliente con exito");
+		fclose(arch4);
+		gotoxy(2,20); system("pause");
+	}
+}
 
 
 main()
